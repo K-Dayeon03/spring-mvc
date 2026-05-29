c376bd06d0113ccacc6133e29b69427eff67bf0a
LeeKM321 <stephen4951@gmail.com>
2026-05-29 10:09:22 +0900

    Initial commit

---
+++ src/main/java/com/codeit/mvc/SpringMvcPractice202605Application.java
@@ -0,0 +1,13 @@
+package com.codeit.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPractice202605Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcPractice202605Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeit/mvc/SpringMvcPractice202605ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPractice202605ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
